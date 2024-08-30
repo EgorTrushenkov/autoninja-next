9c32537103f011c6949a64a015cd28d377184aba
egortrushenkov <etrushenkov@gmail.com>
2024-08-30 16:17:19 +0300

    fix icon and auction work

--- src/components/auc/auc.tsx
+++ src/components/auc/auc.tsx
@@ -9,10 +9,10 @@ export default function Auc() {
                     <h1>У нас на сайте появился раздел “Аукцион”</h1>
                     <div className="info-auc">
                         <p>Переходи на страницу с аукционом и смотри какие автомобили можно купить прямо сейчас!</p>
-                        <a className="button button_2 button-header-mob auc-btn" href="https://auc.auto-ninja.ru/">
+                        <Link className="button button_2 button-header-mob auc-btn" href="/auction">
                             <span>Перейти на аукцион</span>
                             <img src="/images/arrow-auc.svg" alt=""/>
-                        </a>
+                        </Link>
                     </div>
                 </div>
             </div>
